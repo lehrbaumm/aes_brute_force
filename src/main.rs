8daeb32682c5d9a54fdf47b7da2a1e64b427e5dc
Maximilian Lehrbaum <maximilian.lehrbaum@k-business.com>
2023-08-01 16:13:45 +0200

    Dependencies update

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,5 @@
-extern crate rayon;
-extern crate crypto;
-extern crate hex;
-extern crate itertools;
-
 use rayon::prelude::*;
-use itertools::itertools;
+use itertools::Itertools;
 
 fn main() {
     let first_block = "833437EB20E7BF555B9193326DE6E9E9";
@@ -15,8 +10,8 @@ fn main() {
             i
         }).collect::<Vec<u8>>();
     let bruteforce_range: Vec<[u8; 128]> = Vec::new();
-    let combin = bruteforce_range.into_par_iter()
-        .combinations(4).collect::<Vec<[u8;4]>>();
+    let combin: Vec<_> = bruteforce_range.into_iter()
+        .combinations(4).collect();
     println!("{:?}", all_u8);
     println!("Hello, world!");
 }
